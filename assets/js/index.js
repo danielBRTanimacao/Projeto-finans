8ee837446ea4ec2ea2c283d0a7a2fbf6b90a866e
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-11-28 18:20:39 -0300

    config and update indexs

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,9 @@
 function redirectEmail() {
     let text_register = document.querySelector('input#emailtxt')
     if (text_register.value.length == '') {
-        alert('Digite um email valido')
+        alert('Digite um email valido!')
     } else {
-        window.location.href = "http://127.0.0.1:5500/index.html"
-        window.location.assign("http://127.0.0.1:5500/login.html")
+        window.location.assign("http://127.0.0.1:5500/userarea.html")
+        // window.location.assign("https://danielbrtanimacao.github.io/Projeto-finans/userarea.html")
     }
 }
