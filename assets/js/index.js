7ed1e30cef82f2bfdcda7c22582ec68c37fef24e
Daniel Tenório <93400508+danielBRTanimacao@users.noreply.github.com>
2023-11-28 18:21:45 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ function redirectEmail() {
     if (text_register.value.length == '') {
         alert('Digite um email valido!')
     } else {
-        window.location.assign("http://127.0.0.1:5500/userarea.html")
-        // window.location.assign("https://danielbrtanimacao.github.io/Projeto-finans/userarea.html")
+        // window.location.assign("http://127.0.0.1:5500/userarea.html")
+        window.location.assign("https://danielbrtanimacao.github.io/Projeto-finans/userarea.html")
     }
 }
